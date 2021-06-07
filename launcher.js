d7ac4d6dcd0e26af0bd136bfd14b29001df58d09
oliviad2495 <79672161+oliviad2495@users.noreply.github.com>
2021-06-07 16:46:58 -0400

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,27 @@
+class launcher{
+    
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA:bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.launcher = Constraint.create(options);
+        this.pointB = pointB;
+        World.add(world, this.launcher);
+    }
+
+    display(){
+        if(this.launcher.bodyA){
+        var pointA = this.launcher.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+
+    fly(){
+        this.launcher.bodyA = null
+    }
+}
